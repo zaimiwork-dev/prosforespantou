03fc83b444bee44dd8bb64f5c1d0f1cbea14290c
zaimiwork-dev <zaimiwork@gmail.com>
2026-04-13 21:12:53 +0300

    Correct Prisma 7 constructor (datasourceUrl)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,11 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
-    }
+    datasourceUrl: process.env.DATABASE_URL
   });
 };
 
